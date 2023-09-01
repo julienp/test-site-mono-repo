9e85ab02e117573b286c5df8a3868d6affb64983
Julien Poissonnier <julien@caffeine.lu>
2023-09-01 13:53:23 +0200

    Initial

---
+++ one/gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `one`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
---
+++ two/gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `two`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
